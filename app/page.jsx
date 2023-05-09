1b584186848eb928bdd6dc54dca751989d2857ad
Bernardo Bustamante-Martinez <bernardo@bernardos-air.lan>
2023-05-09 14:52:19 -0700

    Render Feed component on Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
- // Home
+ import Feed from '@components/Feed';
+
  const Home = () => {
    return (
      <section className="w-full flex-center flex-col">
@@ -10,7 +11,7 @@
       <p className="desc text-center">
         Promptopedia is an open-source AI prompting tool for the modern world to explore, construct, and share creative prompts
       </p>
-      {/*Feed*/}
+      <Feed />
      </section>
    )  
  } 
